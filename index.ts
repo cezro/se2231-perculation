c0e02bcbbdf396c611e29f00709875220d787fff
cezro <hyderz1218@gmail.com>
2024-01-30 11:18:08 +0800

    refactoring of code

---
+++ index.ts
@@ -0,0 +1,6 @@
+import { UF } from "./src/classes/UF";
+import { Percolation } from "./src/classes/Percolation";
+
+const pc = new Percolation(3);
+
+pc.showCurrent();
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { Canvas } from "./classes/Canvas";
-
-
-const canvas5 = new Canvas(5)
-
-canvas5.show()
-
